117c70afa2c9aa2733e2b7fb2686feea0c7ac9e8
Galugis <david.galugis@gmail.com>
2024-01-17 16:35:20 +0100

    Update monitors.service.ts

--- src/monitors/monitors.service.ts
+++ src/monitors/monitors.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { BaseService } from '../@core/base-service';
 import { InjectRepository } from '@nestjs/typeorm';
